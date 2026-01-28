363a22587eb0aa8c4c37de11a97efa4b0b0adb04
iamsanjayaw-sys <iamsanjaya.w@gmail.com>
2026-01-28 23:41:39 +0530

    Created Button components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import "./Button.css";
+
+export default function Button({ text, onClick, type = "primary" }) {
+  return (
+    <button className={`btn btn-${type}`} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
